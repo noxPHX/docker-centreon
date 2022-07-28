6eccc583a5af0bab1ccddaf95e722468c5637d34
noxPHX <emmanuel.feral@protonmail.com>
2022-07-28 16:08:18 +0200

    Using Compose

--- start.sh
+++ start.sh
@@ -6,17 +6,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-OPTIONS=rw,rprivate,nosuid,nodev
-NAME=centreon-central
-
-docker run --rm -t -d \
-  --tmpfs /tmp:"${OPTIONS}" \
-  --tmpfs /run:"${OPTIONS}" \
-  --tmpfs /run/lock:"${OPTIONS}" \
-  --tmpfs /var/log/journal:"${OPTIONS}" \
-  --cgroupns=private \
-  --name "${NAME}" --hostname "${NAME}" \
-  -p "80:80" \
-  centreon:central
+docker-compose up -d
 
 nsenter -t $(docker inspect -f '{{.State.Pid}}' centreon-central) -m -p -C /bin/sh -c 'umount /sys/fs/cgroup/ && mount -t cgroup2 cgroup2 /sys/fs/cgroup/ -o rw ; pkill sleep'
