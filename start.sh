6de679ec92fc6657285d021063548c5b34e5aa6b
noxPHX <emmanuel.feral@protonmail.com>
2022-08-08 15:30:53 +0200

    Forward SystemD logs to host

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+UUID=$(cat ./machine-id)
+export UUID
+
 docker-compose up -d
 
 nsenter -t "$(docker inspect -f '{{.State.Pid}}' centreon-central)" -m -p -C /bin/sh -c 'umount /sys/fs/cgroup/ && mount -t cgroup2 cgroup2 /sys/fs/cgroup/ -o rw ; pkill sleep'
